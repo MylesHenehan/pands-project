1218dc9787819bb2363b8aef31c345f18e4f16ce
Myles Henehan <mylesjhenehan98@gmail.com>
2024-05-01 20:54:57 +0100

    Creating histograms, editing readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # pands-project
+
+# Investigation of the Iris Dataset
+Author: Myles Henehan
+***
+
+## Project Summary
+This project consists of an exploration of the Iris Data Set, using Python to examine its attributes and form a conclusion about the data contained within. There will be 3 main aims to this project; the first will be to identify the variables present in the data set and export each of them to a single text file. The second approach will be to produce representations of said variables in the form of histograms, and save them as png files. Finally, we will produce a scatter plot of each of the variables, and conclude by providing any other relevant analysis that may be of interest to someone investigating this data set.
+
+## Background of the Dataset
+First introduced in 1936 by British statistician Ronald Fisher in his paper titled "The use of multiple measurements in taxonomic problems," the Iris Data set is a staple in the field of machine learning and statistics, and serves as a simple, straightforward source of data through which beginners in the field of data analytics can build up their knowledge.
+
+The data set contains 150 instances, split evenly between 3 different types (otherwise known as classes) of Iris flower. These types are setosa, versicolor, and virginica. Moreover, each flower has 4 different features (known as variables): sepal length, sepal width, petal length and petal width. This combination of classes and variables gives students in the field of data science a simple yet all-encompassing set of data through which to explore data representation using Python.
+
+## Variables within the Dataset
+
+## Contents of the py file
+
+## References
---
+++ analysis.py
@@ -0,0 +1,66 @@
+# [Placeholder]
+# 1. Outputs a summary of each variable to a single text file,
+# 2. Saves a histogram of each variable to png files, and
+# 3. Outputs a scatter plot of each pair of variables.
+# 4. Performs any other analysis you think is appropriate.
+# Author: Myles Henehan
+
+# Let's start by importing the relevant libraries 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Now we need to read in our dataset
+file = 'iris/iris.data' # we will assign the path of our dataset to the variable "file"
+df = pd.read_csv(file, header=None) # we then use the .read_csv function from pandas to read in our data. Since there is no header, we want the data from the first row, hence "Header=None".
+print(df) # as a test, let's print it. This will result in the first and last five rows being printed.
+
+# Since the dataset doesn't have headers, we will assign each data series to the right variables, using the information in the iris.names file for reference
+sepallen = df[0]
+sepalwidth = df[1]
+petallen = df[2]
+petalwidth = df[3]
+species = df[4]
+
+# I also want to convert them to NumPy arrays, as this will make the data easier to deal with later on.
+#sepallen = sepallen.to_numpy()
+#sepalwidth = sepalwidth.to_numpy()
+#petallen = petallen.to_numpy()
+#petalwidth = petalwidth.to_numpy()
+#species = species.to_numpy()
+
+
+# Creating our histograms
+
+# Sepal Length
+plt.hist(sepallen, bins=10, color='yellow', edgecolor='black') # this function plots a histogram, specifying the data series, the number of bins to sort the data into, and the colour of the edges of the bars.
+plt.xlabel('Sepal length (cm)') # Again, we add our data labels.
+plt.ylabel('No. of Iris Flowers')
+plt.title('Sepal Length of Iris Flowers')
+plt.savefig('sepallen_histogram.png')
+
+# Sepal Width
+plt.hist(sepalwidth, bins=10, color='yellow', edgecolor='black')
+plt.xlabel('Sepal width (cm)') 
+plt.ylabel('No. of Iris Flowers')
+plt.title('Sepal Width of Iris Flowers')
+plt.savefig('sepalwidth_histogram.png')
+
+# Petal Length
+plt.hist(petallen, bins=10, color='yellow', edgecolor='black')
+plt.xlabel('Petal length (cm)') 
+plt.ylabel('No. of Iris Flowers')
+plt.title('Petal Length of Iris Flowers')
+plt.savefig('petallen_histogram.png')
+
+# Petal Width
+plt.hist(petalwidth, bins=10, color='yellow', edgecolor='black')
+plt.xlabel('Petal width (cm)') 
+plt.ylabel('No. of Iris Flowers')
+plt.title('Petal Width of Iris Flowers')
+plt.savefig('petalwidth_histogram.png')
+
+
+
+
+
