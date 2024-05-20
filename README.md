3d77cd7c5a43afb0edadae72664cfd6374618f28
Myles Henehan <mylesjhenehan98@gmail.com>
2024-05-20 16:34:59 +0100

    Updating ReadMe and making small improvements

--- README.md
+++ README.md
@@ -17,24 +17,25 @@ The data set contains 150 instances of iris flower, with 50 of each of three spe
 The main component of this project is a Python file called analysis.py, which carries out a number of commands to help us explore the Iris data set. It is also accompanied by a file named functions.py, which stores the main functions created for this project and used in the analysis.py file.
 
 ### 1. Reads in the dataset and assigns each data frame to a variable name
-Since the dataset itself does not have headers, we are using the information in the iris.names file for reference.
+Since the dataset itself does not have headers, I have used the information in the iris.names file for reference. I have also created a list of tuples, with each tuple containing the name of each data frame and the data frame itself (for numerical variables only).
 
 ### 2. Writes a description of each of the 4 main numerical variables and stores this in a single txt file.
+To do this step, I have used a custom made function, datasummary, which iterates over our list of tuples and uses the describe function from the pandas library to get important statistical data such as the mean, the standard deviation and the min and max values. Our function then writes this data to a txt file (irisdatasummary.txt) in a readable format.
 
 ### 3. Outputs histograms to show the spread of each numerical variable and saves these as png files.
+For step 3, I have created another function, irisdatahist, which iterates over our tuple list and creates a separate histogram for each of our numerical variables. Our function then saves each histogram as a png file. For this step, matplotlib is extremely important as it offers functions which help us to visualise our data.
 
 ### 4. Creates a set of scatter plots, showing the relationship between each possible combination of variables.
+In this step, I first used a function from the module itertools, called combinations. This function generates all the possible pairs of variables (in this case, 6). I then used our custom function, irisscatter, to create a scatter plot for each pair.
 
-### 5. Prompts the user to read in 2 variables, and gets the best fit line between them to offer a visualisation of the trend.
- - When looking at scatter plots, it can often be useful to work out the best fit line to visualise the overall trend in the data. For example, when you look at Petal Length versus Petal Width above, you can see quite a linear progression of the data. Let's use a best fit line to demonstrate this. In order to plot this, we will first need to determine the slope of the line and the point at which it intercepts the y axis, and then use the y = mx + c method.
+### 5. Prompts the user to read in 2 variables, and gets the best fit line and Pearson Correlation Coefficient
+Having completed the main aims of this project, I wanted to make my programme more interactive, so I wrote a code that prompts the user to select 2 variables they would be interested in seeing the best fit line for. Using my custom function, bestfit, the programme then tells the user the slope and intercept for their inputted variables, and creates a scatter plot with the best fit line over it. Then, using the corr function from pandas, the programme calculates the Pearson Correlation Coefficient for this pair of variables and tells the user what kind of correlation this indicates. This method measures the extent to which the points in a scatter plot cluster around a straight line, and is widely used in statistics and data analysis to assess the relationship between two continuous variables. This coefficient can range between 0 and 1, with 0 implying no correlation, 1 implying a very positive correlation, and -1 implying a very negative correlation (Turney, 2022).
 
-### 6. Gets the Pearson correlation coefficient of said 2 variables
-This method measures the extent to which the points in a scatter plot cluster around a straight line, and is widely used in statistics and data analysis to assess the relationship between two continuous variables. This coefficient can range between 0 and 1, with 0 implying no correlation, 1 implying a very positive correlation, and -1 implying a very negative correlation (Turney, 2022).
-
-### 7. Addresses the effect of the categorical variable species by creating a representation which includes this as a third variable.
-Sometimes when plotting data, the data can appear in clusters. In this case, it is common that a categorical variable is affecting the data. Let's look at Petal Length histogram as an example. [Placeholder]
+### 6. Addresses the effect of the categorical variable "species" by creating a representation which includes this as an added variable.
+Finally, I wanted to showcase the effect that a hidden categorical variable can have on the spread of data. For this, I have used Petal Length as an example, as the high standard deviation in this data indicates another variable at play. Sure enough, when we plot the petal length values grouped by species, we can see that setosas tend to have a much smaller petal length, while virginicas tend to have the longest petals.
 
 ## Conclusion
+Through this project, we've attained an in-depth grasp of the attributes of the Iris dataset, employing Python to conduct diverse analyses and visualisations. The project successfully accomplished several goals, encompassing variable identification and description, histogram and scatter plot creation, and explorations of additional areas such as the best fit line, the Pearson correlation coefficient, and the impact of hidden variables. This exploration has yielded significant insights into data analysis methodologies and techniques, underscoring Python's efficacy as a tool for navigating and comprehending complex datasets.
 
 ## References
 - Fisher,R. A.. (1988). Iris. UCI Machine Learning Repository. https://doi.org/10.24432/C56C76.
--- analysis.py
+++ analysis.py
@@ -1,14 +1,14 @@
 # Analysis of the Iris Dataset
 # Author: Myles Henehan
 
-# Importing the relevant libraries for this project
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import itertools as it # this module is very helpful as it contains the .combinations() function, which allows us to get all the combinations of our variables. We will use this for our scatter plots.
+# Importing the relevant libraries/modules for this project
+import pandas as pd # we will need this for a range of functions across the project
+import matplotlib.pyplot as plt # we will need this for plotting our data.
+import numpy as np # we will need this for the polyfit() function that we use for the line of best fit
+import itertools as it # this module is very helpful as it contains the .combinations() function, which allows us to get all the combinations from our variable list. We will use this for our scatter plots.
 
 # Reading in our dataset
-file = 'iris/iris.data' # we will assign the path of our dataset to the variable "file"
+file = 'iris/iris.data' # we first assign the path of our dataset to the variable "file"
 df = pd.read_csv(file, header=None) # we then use the .read_csv function from pandas to read in our data. 
 # Since there are no headers, we want the data from the first row, hence "Header=None".
 print(df) # as a test, let's print it. This will result in the first and last five rows being printed.
@@ -20,7 +20,7 @@ petallen = df[2]
 petalwidth = df[3]
 species = df[4]
 
-# let's first make a list of our data frames so they are easy to manipulate.
+# let's then make a list of our data frames so they are easy to manipulate.
 variablelist = [
     ("Sepal Length", sepallen), 
     ("Sepal Width", sepalwidth), 
@@ -31,21 +31,16 @@ variablelist = [
 
 # Project Aim 1: Output a summary of each variable to a single text file.
 
-# To simplify this, I have created a function called datasummary, which uses the describe() function.
+# To simplify this, I have created a function called datasummary, which employs the describe() function to summarise the data and saves it to a txt.
 
 from irisfunctions import datasummary 
 datasummary(variablelist)
 
 
 
-
-
-
 # Project Aim 2: Save a Histogram of each variable to png files.
 
-# To simplify this, I have created a function called irisdatahist.
-# This function reads in the variable, the x label, the title of the histogram and the file name to be saved to. It creates a histogram and saves it as a png with this file name.
-
+# To simplify this, I have created a function called irisdatahist. This function reads in a list of tuples and iterates over it to create a histogram for each variable.
 from irisfunctions import irisdatahist
 irisdatahist(variablelist)
     
@@ -55,21 +50,26 @@ irisdatahist(variablelist)
 
 
 # Project Aim 3: Output a scatter plot of each pair of variables
-varcombinations = it.combinations(variablelist, 2)
+# To simplify this, I have created a function called irisscatter, which takes in all combinations of pairs generated from our variable list, and creates a scatter plot of each combination.
+
+varcombinations = it.combinations(variablelist, 2) # this function generates all combinations of length 2 (i.e. pairs) from the list we provided.
 
 from irisfunctions import irisscatter
-irisscatter(varcombinations)
+irisscatter(varcombinations) # now we can use our irisscatter function, reading in the varcombinations to get our 6 possible scatter plots.
 
 
 
-# Project Aim 4: Other useful analyses
+# Project Aim 4: Other useful analyses...
 
-# Best Fit Line
+# A. Best Fit Line
+# To simplify this, I have created a function called bestfit, which reads in 2 variables and plots them in a scatter plot with the best fit line plotted on top.
 from irisfunctions import bestfit
-print("To see the best fit line for a combination of variables, please choose the corresponding numbers.")
-x = input("Please input your first variable: \n\n1.Sepal Length\n\n2.Sepal Width\n\n3.Petal Length\n\n4.Petal Width\n\n")
+print("To see the best fit line for a combination of variables, please select the corresponding number for each variable.") # first, we give the user instructions.
+
+x = input("Please input your first variable: \n\n1.Sepal Length\n\n2.Sepal Width\n\n3.Petal Length\n\n4.Petal Width\n\n") # then we ask them to input each variable by typing the corresponding number.
 y = input("\n\nPlease input your second variable: \n\n1.Sepal Length\n\n2.Sepal Width\n\n3.Petal Length\n\n4.Petal Width\n\n")
 
+# Below, we use an if condition to ensure that for whichever number the user selects, the right data frames are used for the x and y values.
 if x == str(1):
     xvar = petallen
 elif x == str(2):
@@ -78,6 +78,8 @@ elif x == str(3):
      xvar = sepallen
 elif x == str(4):
     xvar = sepalwidth
+else:
+    raise ValueError("Invalid input for the first variable. Please enter a number between 1 and 4.") # Additionally, I want to let the user know if they enter an incorrect number.
 
 if y == str(1):
     yvar = petallen
@@ -87,22 +89,42 @@ elif y == str(3):
     yvar = sepallen
 elif y == str(4):
     yvar = sepalwidth
+else:
+    raise ValueError("Invalid input for the second variable. Please enter a number between 1 and 4.")
 
-bestfit(xvar,yvar)
-
-
-# Pearson Correlation Coefficient
-r = xvar.corr(yvar) # .corr() is a pandas function which calculates correlation between two data frames, using the Pearson correlation coefficient by default.
-print("Pearson correlation coefficient:", r)
+bestfit(xvar,yvar) # finally, we use these values as the parameters for our function in order to get a plot with the best fit line for whichever plot the user would like to see.
 
 
+# B. Pearson Correlation Coefficient
+r = xvar.corr(yvar) # .corr() is a pandas function which calculates correlation between two data frames, using the Pearson correlation coefficient by default (insert source).
+print(f"The Pearson correlation coefficient for this pair of variables is {r}")
 
-# The hidden variable
-grouped = df.groupby(4) # The easiest way to showcase this is to use a .groupby() function (OpenAI, 2024)
-for species, data in grouped:
-    plt.hist(data[2], bins=10, alpha=0.5, label=species)
+# We then use an if statement to tell the user if their chosen variables display a positive or negative correlation:
+if r >= 0.7:
+    print("This indicates a strong positive correlation.")
+elif 0.3 <= r < 0.7:
+    print("This indicates a moderate positive correlation.")
+elif 0 < r < 0.3:
+    print("This indicates a weak positive correlation.")
+elif r <= -0.7:
+    print("This indicates a strong negative correlation.")
+elif -0.7 < r <= -0.3:
+    print("This indicates a moderate negative correlation.")
+elif -0.3 < r < 0:
+    print("This indicates a weak negative correlation.")
+else:
+    print("There is no correlation.")
+
+
+
+# C. The hidden variable
+# The easiest way to showcase categorical variables affecting the data is to use a .groupby() function (OpenAI, 2024)
+df.columns = ['sepallen', 'sepalwidth', 'petallen', 'petalwidth', 'species'] # I quickly added the headings of our data frames to a simple list to make the code below more clear.
+grouped = df.groupby('species') # this function splits the data into groups according to some criteria, in this case by the types in data frame 4 (species)
+for species, data in grouped: # this line iterates over the pairs of variables (species and data).
+    plt.hist(data['petallen'], bins=10, alpha=0.5, label=species) # in this case, I want to look at Petal Length, so I'm using 2 as the index.
 plt.legend()
-plt.xlabel('Petal Length')
-plt.ylabel('Frequency')
-plt.title('Histogram of Petal Length by Species')
-plt.show()
+plt.xlabel("Petal Length")
+plt.ylabel("Frequency")
+plt.title("Histogram of Petal Length by Species")
+plt.show() # after putting in our labels, legend, and title, now we can take a look at the histogram to see how the third variable affects our spread.
